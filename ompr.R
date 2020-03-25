dd17fb7250b541b9d2cc3786e8c3a171a5a7d076
Rahul Roy <rahulroynit@gmail.com>
2020-03-25 18:05:14 +0530

    update commit

--- ompr.R
+++ ompr.R
@@ -32,7 +32,7 @@ w <- list(c(3, 4), c(3, 4), c(-2, 1))
 
 # lp model
 model <- MIPModel()%>%
-  add_variable(x[i], i = 1:n, type = "continuous")%>%
+  add_variable(x[i], i = 1:n, type = "integer")%>%
   set_objective(sum_expr(ofc[[i]]*x[i], i = 1:n), "max")%>%
   add_constraint(sum_expr(w[[1]][i]*x[i], i = 1:n) <= 100)%>%
   add_constraint(sum_expr(w[[2]][i]*x[i], i = 1:n) >= 10)%>%
@@ -48,3 +48,40 @@ model%>%
   objective_value()
 
 ## ---------------------------------------------------------------------------------------------
+
+## knapsack problem
+# number of items: n = 10
+# each item has a weight = pi, i is the item index
+# value of each item = ci, i is the item index
+# total capacity of cargo (in weight): W = 35 units
+# we have 10 decision variables: x[i], i = 1 to 10
+# decision variable is a binary variable
+# x[i] = 0 is item is not packed, else x[i] = 1
+
+# number of decision variables
+n <- 10
+
+# value of items
+c <- list(16, 22, 12, 8, 15, 17, 14, 16, 20, 21)
+
+# weight of items
+p <- list(5, 7, 4, 3, 6, 5, 7, 8, 4, 6)
+
+# knapsack model (mip)
+model <- MIPModel()%>%
+  add_variable(x[i], i = 1:n, type = "binary")%>%
+  set_objective(sum_expr(c[[i]]*x[i], i = 1:n), "max")%>%
+  add_constraint(sum_expr(p[[i]]*x[i], i = 1:n) <= 35)%>%
+  solve_model(with_ROI(solver = "glpk"))
+
+# optimum solution
+model%>%
+  get_solution(x[i])
+
+# objective function value
+model%>%
+  objective_value()
+
+## ---------------------------------------------------------------------------------------------
+
+
