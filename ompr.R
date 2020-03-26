e042b5932eef43705517d76717f434b4b843c1c4
Rahul Roy <rahulroynit@gmail.com>
2020-03-26 21:21:02 +0530

    update commit

--- ompr.R
+++ ompr.R
@@ -67,7 +67,7 @@ c <- list(16, 22, 12, 8, 15, 17, 14, 16, 20, 21)
 # weight of items
 p <- list(5, 7, 4, 3, 6, 5, 7, 8, 4, 6)
 
-# knapsack model (mip)
+# knapsack model (ip)
 model <- MIPModel()%>%
   add_variable(x[i], i = 1:n, type = "binary")%>%
   set_objective(sum_expr(c[[i]]*x[i], i = 1:n), "max")%>%
@@ -77,6 +77,51 @@ model <- MIPModel()%>%
 # optimum solution
 model%>%
   get_solution(x[i])
+
+# objective function value
+model%>%
+  objective_value()
+
+## ---------------------------------------------------------------------------------------------
+
+## assignment problem
+# p jobs and p machines
+# only one job can be processed on one machine
+# processing job i on machine j costs cij
+# assign jobs to minimize cost
+
+# number of jobs = number of machines = number of decision variables
+p <- 5
+
+# cost matrix
+# column cj = cost of doing job on machine j
+# ith row of cj = cost of doing job i on machine j 
+cost <- data.frame(c1 = c(15, 12, 14, 13, 16),
+                c2 = c(10, 25, 7, 15, 16),
+                c3 = c(11, 7, 9, 15, 14),
+                c4 = c(10, 12, 8, 9, 11),
+                c5 = c(11, 15, 8, 7, 6)) 
+
+# cost of doing job i on machine j
+i <- 3; j <- 5
+cost[i, j]
+
+# assignment model (ip)
+model <- MIPModel()%>%
+  # x[i, j] = 1 if job i gets assigned to machine j
+  add_variable(x[i, j], i = 1:p, j = 1:p, type = "binary")%>%
+  # minimize the cost of doing all jobs on all machines
+  set_objective(sum_expr(cost[i, j] * x[i, j], i = 1:p, j = 1:p), "min")%>%
+  # each job gets assigned to only one machine  
+  add_constraint(sum_expr(x[i, j], j = 1:p) == 1, i = 1:p)%>%
+  # each machine gets assigned to only one job
+  add_constraint(sum_expr(x[i, j], i = 1:p) == 1, j = 1:p)%>%
+  solve_model(with_ROI(solver = "glpk"))
+
+# optimum solution
+model%>%
+  get_solution(x[i, j])%>%
+  filter(value > 0)
 
 # objective function value
 model%>%
